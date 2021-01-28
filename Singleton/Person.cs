d95df6117816e84fdb961420d27527e86c07b201
Dolejš Daniel <dolejs.daniel.2017@skola.ssps.cz>
2021-01-28 22:25:26 +0100

    Přidejte soubory projektu.

---
+++ Singleton/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Person
+    {
+        public Person(string name, string surname, string bnum, DateTime bdate)
+        {
+            Name = name;
+            Surname = surname;
+            Bnum = bnum;
+            Bdate = bdate;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Bnum{ get; set; }
+        public DateTime Bdate { get; set; }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        public Dictionary<string, Person> persons = new Dictionary<string, Person>();
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+            persons.Add("010213/0812", new Person("John", "Doe", "020620/0269", new DateTime(2002, 6, 20)));
+            persons.Add("010213/0812", new Person("Jane", "Doe", "020621/0270", new DateTime(2002, 6, 21)));
+            ;
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
